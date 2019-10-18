f1eee470fccf24e2a21f0f01d3ec1e0d5a5ea0ae
ThallesBogar <thallesbogar@gmail.com>
2019-10-18 16:16:31 -0300

    Implementação Map-Reduce

--- database/fazenda.lisp
+++ database/fazenda.lisp
@@ -6,10 +6,10 @@
 (in-package :fazenda)
 
 
-(start (make-instance 'easy-acceptor :port 4242))
+#|(start (make-instance 'easy-acceptor :port 8080))
 (push (create-folder-dispatcher-and-handler "/html/"
-					    #p"/home/ubuntu/wrk/html/")
-      *dispatch-table*)
+					    #p"/home/thalles/wrk/html/")
+      *dispatch-table*)|#
 
 (cl-mongo:db.use "fazenda")
 
@@ -19,9 +19,29 @@
 (defvar *manejo-collection* "manejo")
 (defvar *trato-collection* "trato")
 (defvar *manutencao-collection* "manutencao")
+(defvar *mr-compra-collection* "mr.compra")
+(defvar *mr-venda-collection* "mr.venda")
+(defvar *mr-morte-collection* "mr.morte")
+(defvar *mr-manejos-collection* "mr.manejo.saida")
+(defvar *mr-manejoe-collection* "mr.manejo.entrada")
+(defvar *acceptor* NIL)
+
+(defun start-server (port)
+  (setf *acceptor* (make-instance 'easy-acceptor :port port))
+  (start *acceptor*)
+  (push (create-folder-dispatcher-and-handler "/html/"
+					      #p"/home/thalles/wrk/html/")
+	*dispatch-table*))
+
+(defun stop-server ()
+  (stop *acceptor* :soft NIL))
+
 
 (defclass compra ()
-  ( (data         :accessor data
+  ( (_id          :accessor _id
+		  :initarg :_id)
+
+    (data         :accessor data
 	          :initarg :data)
     
     (quantidade   :accessor quantidade
@@ -110,7 +130,14 @@
 	   :initarg :tipo)
 
     (pasto :accessor pasto
-	   :initarg :pasto))) 
+	   :initarg :pasto)))
+
+(defclass mr ()
+  ( (_id   :accessor _id
+	   :initarg :_id)
+
+    (value :accessor value
+	   :initarg :value)))
 
 
 
@@ -162,6 +189,9 @@
      ($ "tipo"  (tipo trato))
      ($ "pasto" (pasto trato))))
 
+(defun mr->doc (mr)
+  ($ ($ "idade" (idade mr))
+     ($ "quantidade" (quantidade mr))))
 
 
 (defun add-compra (data quantidade pastodestino idade vendedor)
@@ -172,6 +202,28 @@
  				:idade        idade
 				:vendedor     vendedor)))
     (db.insert *compra-collection* (compra->doc compra))))
+
+(defun add-compra2 ()
+  (let ( (compra (make-instance 'compra
+				:data         "teste"
+				:quantidade   10
+				:pastodestino "teste2"
+ 				:idade        "teste3"
+				:vendedor     "teste4")))
+    (compra->doc compra)))
+
+(defun teste ()
+  (let ( (teste (make-instance 'compra
+			       :data "10/12/2018"
+			       :quantidade 50
+			       :pastodestino "pastoteste"
+			       :idade "18meses"
+			       :vendedor "thalles")))
+    (list :data (data teste)
+	  :quantidade (quantidade teste)
+	  :pastodestino (pastodestino teste)
+	  :idade (idade teste)
+	  :vendedor (vendedor teste))))
 
 
 
@@ -230,7 +282,7 @@
   (let ( (found-compra (docs (db.find *compra-collection* :all))))
     (with-output-to-string (stream)
 			   (html-template:fill-and-print-template
-			    #P"/home/ubuntu/wrk/html/compraTable.html"
+			    #P"/home/thalles/wrk/html/compraTable.html"
 			    (list :historicoCompra
 				  (loop for compra in found-compra collect
 					(let ( (compra-object (make-instance 'compra
@@ -252,7 +304,7 @@
   (let ( (found-venda (docs (db.find *venda-collection* :all))))
     (with-output-to-string (stream)
 			   (html-template:fill-and-print-template
-			    #P"/home/ubuntu/wrk/html/vendaTable.html"
+			    #P"/home/thalles/wrk/html/vendaTable.html"
 			    (list :historicoVenda
 				  (loop for venda in found-venda collect
 					(let ( (venda-object (make-instance 'venda
@@ -274,7 +326,7 @@
   (let ( (found-manejo (docs (db.find *manejo-collection* :all))))
     (with-output-to-string (stream)
 			   (html-template:fill-and-print-template
-			    #P"/home/ubuntu/wrk/html/manejoTable.html"
+			    #P"/home/thalles/wrk/html/manejoTable.html"
 			    (list :historicoManejo
 				  (loop for manejo in found-manejo collect
 					(let ( (manejo-object (make-instance 'manejo
@@ -296,7 +348,7 @@
   (let ( (found-morte (docs (db.find *morte-collection* :all))))
     (with-output-to-string (stream)
 			   (html-template:fill-and-print-template
-			    #P"/home/ubuntu/wrk/html/morteTable.html"
+			    #P"/home/thalles/wrk/html/morteTable.html"
 			    (list :historicoMorte
 				  (loop for morte in found-morte collect
 					(let ( (morte-object (make-instance 'morte
@@ -318,7 +370,7 @@
   (let ( (found-trato (docs (db.find *trato-collection* :all))))
     (with-output-to-string (stream)
 			   (html-template:fill-and-print-template
-			    #P"/home/ubuntu/wrk/html/tratoTable.html"
+			    #P"/home/thalles/wrk/html/tratoTable.html"
 			    (list :historicoTrato
 				  (loop for trato in found-trato collect
 					(let ( (trato-object (make-instance 'trato
@@ -336,7 +388,7 @@
   (let ( (found-manutencao (docs (db.find *manutencao-collection* :all))))
     (with-output-to-string (stream)
 			   (html-template:fill-and-print-template
-			    #P"/home/ubuntu/wrk/html/manutencaoTable.html"
+			    #P"/home/thalles/wrk/html/manutencaoTable.html"
 			    (list :historicoManutencao
 				  (loop for manutencao in found-manutencao collect
 					(let ( (manutencao-object (make-instance 'manutencao
@@ -348,8 +400,20 @@
 						:pasto (pasto manutencao-object)))))
 			    :stream stream))))
 
-
-
+(defun sum-gado ()
+  (let ( (found-mr-compra (docs (db.find *mr-compra-collection*   :all)))
+	 (found-mr-venda (docs (db.find *mr-venda-collection*     :all)))
+	 (found-mr-morte (docs (db.find *mr-morte-collection*     :all)))
+	 (found-mr-manejos (docs (db.find *mr-manejos-collection* :all)))
+	 (found-mr-manejoe (docs (db.find *mr-manejoe-collection* :all))))
+    (loop for mr-compra in found-mr-compra collect
+	  (let( (mr-compra-object (make-instance 'mr
+						 :_id   (get-element "_id" mr-compra)
+						 :value (get-element "value" mr-compra))))
+	    (list :_id   (_id mr-compra-object)
+		  :value (value mr-compra-object))))))
+  
+  
 
 (define-easy-handler (compraHandler :uri "/compra") (data quantidade pastodestino idade vendedor)
   (when (and data quantidade pastodestino idade vendedor)
@@ -359,19 +423,19 @@
 
 
 (define-easy-handler (vendaHandler :uri "/venda") (data quantidade pastosaida idade comprador)
-  (add-venda data quantidade pastosaida idade comprador)
+  (add-venda data (read-from-string quantidade) pastosaida idade comprador)
   (redirect "/html/fazenda.html"))
 
 
 
 (define-easy-handler (morteHandler :uri "/morte") (data causa quantidade pastosaida idade)
-  (add-morte data causa quantidade pastosaida idade)
+  (add-morte data causa (read-from-string quantidade) pastosaida idade)
   (redirect "/html/fazenda.html"))
 
 
 
 (define-easy-handler (manejoHandler :uri "/manejo") (data pastodestino quantidade idade pastosaida)
-  (add-manejo data pastodestino quantidade idade pastosaida)
+  (add-manejo data pastodestino (read-from-string quantidade) idade pastosaida)
   (redirect "/html/fazenda.html"))
 
 
@@ -418,15 +482,43 @@
 
 
 
-#||(defjs map_pasto()
-  (emit this.pastodestino this.quantidade))
+(defjs map-func ()
+  (emit this.idade this.quantidade))
+
+(defjs reduce-func (k vals)
+  (let ((sum 0))
+    (dolist (c vals)
+      (incf sum c))
+    (return sum)))
 
 
-(defjs sum_gado (c vals)
-  ((@ *Array sum) vals))
+(defun mr-compra ()
+  (pp (mr.p ($map-reduce *compra-collection* map-func reduce-func
+			 :out "mr.compra"
+			 :query ($ "pastodestino" "Alto Zé Doroteia")))))
 
+;   Pasto_Total_Gado = (Pasto_Compra_Gado + Pasto_Manejo_Entrada_Gado) - (Pasto_Venda_Gado + Pasto_Morte_Gado + Pasto_Manejo_Saída_Gado)
 
-(defun sum ()
-  (pp (mr.p ($map-reduce *compra-collection*  map_pasto sum_gado))))||#
+(defun mr-venda ()
+  (pp (mr.p ($map-reduce *venda-collection* map-func reduce-func
+			 :out "mr.venda"
+			 :query ($ "pastosaida" "Alto Zé Doroteia")))))
+(defun mr-venda ()
+  (pp (mr.p ($map-reduce *venda-collection* map-func reduce-func
+			 :out "mr.venda"
+			 :query ($ "pastosaida" "Alto Zé Doroteia")))))
 
+(defun mr-morte ()
+  (pp (mr.p ($map-reduce *morte-collection* map-func reduce-func
+			 :out "mr.morte"
+			 :query ($ "pastosaida" "Alto Zé Doroteia")))))
 
+(defun mr-manejo-entrada ()
+  (pp (mr.p ($map-reduce *manejo-collection* map-func reduce-func
+			 :out "mr.manejo.entrada"
+			 :query ($ "pastodestino" "Alto Zé Doroteia")))))
+
+(defun mr-manejo-saida ()
+  (pp (mr.p ($map-reduce *manejo-collection* map-func reduce-func
+			 :out "mr.manejo.saida"
+			 :query ($ "pastosaida" "Alto Zé Dorotéia")))))
